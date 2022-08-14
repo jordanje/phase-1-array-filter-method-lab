1b0a6d284ecdc1d89daf35518e222bacc564ab07
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-14 14:06:09 -0400

    commit tree 63effca659b80ad34138bc50a506e0aa6f34e0e0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array, name) {
+    //returns all driver matching name.toLowerCase()
+    //if no drivers ==> []
+    return array.filter((element) => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => element.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(array, string) {
+    return array.filter(element => element.name === string)
+}
